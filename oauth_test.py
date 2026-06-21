671e6eb6cc01245a03f08ea6414a635868c360bd
T.Fukushima <tk44fk40@gmail.com>
2026-06-22 05:02:57 +0900

    docs/meta: 著作権表記の所有者を tk44 から tk44fk40 に変更
    LICENSE およびすべてのソースコード内の著作権表記（Copyright）の所有者名を GitHub ユーザー名である tk44fk40 に統一・修正しました。

--- oauth_test.py
+++ oauth_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2026 tk44
+# Copyright 2026 tk44fk40
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
