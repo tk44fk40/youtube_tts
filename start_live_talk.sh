f4fefb401c91c872973f587fdc2cbdc5a654a644
T.Fukushima <tk44fk40@gmail.com>
2026-08-09 05:20:04 +0900

    refactor: start_live_talk.sh の引数指定をワンライナーに整理
    - 複数行に分割されていた起動オプションを一行にまとめて記述を簡素化

--- start_live_talk.sh
+++ start_live_talk.sh
@@ -2,6 +2,4 @@
 export OBS_WEBSOCKET_PASSWORD=000000
 uv run youtube_live_voicevox.py \
 --tts-test ライブ配信が開始されました。これは読上げ音声のテストです。 \
---quota-talk \
---backlog-seconds -1 \
-$@
+--quota-talk --backlog-seconds -1 $@
